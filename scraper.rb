2844d36f4b1cc18c3932ecd1d652767bf2b674c5
Tony Bowden <tony@tmtm.com>
2017-01-08 10:47:46 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,23 +11,23 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def scrape_list(url)
   noko = noko_for(url)
   links = noko.css('#block-views-ledamoter-pages-party-block a[href*="ledamoter"]/@href').map(&:text)
-  raise "No links found" unless links.any?
+  raise 'No links found' unless links.any?
   links.each do |link|
     scrape_mp(URI.join(url, link))
   end
- end
+end
 
 def scrape_mp(url)
   noko = noko_for(url)
@@ -36,19 +37,19 @@ def scrape_mp(url)
   contact = noko.css('#block-views-ledamot-detaljer-block-1')
 
   named = ->(t) { box.xpath("(.//strong[contains(.,'#{t}')] | .//b[contains(.,'#{t}')])/following-sibling::text()") }
-  data = { 
-    id: url.to_s.split("/").last,
-    name: noko.css('h1').text.tidy,
-    image: noko.css('img[typeof="foaf:Image"]/@src').text.sub(/\?.*/,''),
-    email: contact.css('a[href*="mailto:"]').text,
-    phone: contact.css('a[href*="tel:"]').text,
-    party: om.xpath('.//span[strong[.="Lagtingsgrupp:"]]/following-sibling::span//text()').first.text.tidy,
+  data = {
+    id:         url.to_s.split('/').last,
+    name:       noko.css('h1').text.tidy,
+    image:      noko.css('img[typeof="foaf:Image"]/@src').text.sub(/\?.*/, ''),
+    email:      contact.css('a[href*="mailto:"]').text,
+    phone:      contact.css('a[href*="tel:"]').text,
+    party:      om.xpath('.//span[strong[.="Lagtingsgrupp:"]]/following-sibling::span//text()').first.text.tidy,
     birth_date: om.xpath('.//span[strong[.="Född:"]]/following-sibling::span//text()').first.text.tidy,
-    term: 2015,
-    source: url.to_s,
+    term:       2015,
+    source:     url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('http://www.lagtinget.ax/ledamoter')
