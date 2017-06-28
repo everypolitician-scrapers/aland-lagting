a9401b51b505007a1280ec1488667f08e90edae2
Tony Bowden <tony@tmtm.com>
2017-06-28 07:20:10 +0100

    Switch to HTTPS

--- scraper.rb
+++ scraper.rb
@@ -48,4 +48,4 @@ def scrape_mp(url)
 end
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list('http://www.lagtinget.ax/ledamoter')
+scrape_list('https://www.lagtinget.ax/ledamoter')
