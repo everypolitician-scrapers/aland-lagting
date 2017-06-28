b276c3ae076ea298b7b490e1670a76b1ff579480
Tony Bowden <tony@tmtm.com>
2017-06-28 07:19:21 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -43,6 +43,7 @@ def scrape_mp(url)
     source:     url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
   ScraperWiki.save_sqlite(%i(id term), data)
 end
 
